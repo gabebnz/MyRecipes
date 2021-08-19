891e962624ddc57065d758a096699bed5ef300cc
test10cs <tcs1016@gmail.com>
2021-08-20 10:32:27 +1200

    url direct to other tab

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyRecipes' });
 });
 
+/* GET mealplanner page. */
+router.get('/mealplanner', function(req, res, next) {
+  res.render('mealplanner', { title: 'Meal Planner' });
+});
+
+/* GET shoppinglist page. */
+router.get('/shoppinglist', function(req, res, next) {
+  res.render('shoppinglist', { title: 'Shopping List' });
+});
+
 module.exports = router;
